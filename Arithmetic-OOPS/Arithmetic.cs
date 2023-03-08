8e994e56868ee94119df3ff4cf09d732f16dccfb
Karpe Aditya Mohan <Karpe Aditya Mohan@LAPTOP-GRP69F4I>
2023-03-08 15:27:23 +0530

    Add project files.

---
+++ Arithmetic-OOPS/Arithmetic.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arithmetic_OOPS
+{
+    internal class Arithmetic
+    {
+
+        // Simple Pameter
+        public void Addition(int num1, int num2)   // Paremeters or Arguments
+        {
+            int result = num1 + num2;
+
+            // Console.WriteLine("Addition of" + num1 + "and" + num2 + "is=" + result );    //before concatenation
+
+            Console.WriteLine($"Addition of {num1} and {num2} is= {result}");   //Concatenation // String Interpulation
+        }
+
+ 
+        // Complex Parameter
+        public int A { get; set; }                // We have to create properties for complex parameters
+        public int B { get; set; }
+        public void Subtraction (Arithmetic arth)    
+        {
+            int result = arth.A - arth.B;
+            Console.WriteLine($"Substraction of {arth.A} and {arth.B} is= {result}");
+
+        }
+
+        //Output Parameter
+        public void Multiplication(int num1, int num2, out int result)   // write out before int result
+        {
+             result = num1 * num2;
+            
+        }
+
+        //Input-Output Parameter
+        public void Division(int num1, int num2, ref int result)   // write ref before int result for input-output parameter...input is default for num1 and num2
+        {
+             result = num1 / num2;
+
+            Console.WriteLine($"Division of {num1} and {num2} is= {result}");
+        }
+
+    }
+}
---
+++ Arithmetic-OOPS/Program.cs
@@ -0,0 +1,24 @@
+﻿using Arithmetic_OOPS;
+
+public class Program
+{
+    public static void Main()
+    {
+        Arithmetic arithmetic1 = new Arithmetic();            //classname.objectname = new object
+        arithmetic1.Addition(10, 20);                         //objectname.methodname 
+
+
+        Arithmetic arithmetic2 = new Arithmetic();
+        arithmetic2.A = 20;
+        arithmetic2.B = 10;
+        Arithmetic arithmetic3 = new Arithmetic();
+        arithmetic3.Subtraction(arithmetic2);
+
+                                
+        arithmetic1.Multiplication(10, 20, out int result);
+        Console.WriteLine($"Multiplication of 10 and 20 is= {result}");
+
+        int result1=10;                                   //we have to declare int as result1 beacause result is already in use above.
+        arithmetic1.Division(20, 10, ref result1);    
+    }
+}
